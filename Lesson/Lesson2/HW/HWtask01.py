14d94ae818290bbd08b4d6dd774606f4a2ced265
denisganiev3354 <denisganiev3354@gmail.com>
2023-05-28 14:12:50 +0400

    (L2)HWtask01

--- Lesson/Lesson2/HW/HWtask01.py
+++ Lesson/Lesson2/HW/HWtask01.py
@@ -2,21 +2,31 @@
 # а некоторые – гербом. Определите минимальное число монеток, которые нужно перевернуть,
 # чтобы все монетки были повернуты вверх одной и той же стороной.
 # Выведите минимальное количество монет, которые нужно перевернуть
+#
+# n = int(input())
+# eagle_count = tails_count = 0
+# for n in range(n):
+#     x = int(input())
+#     if x:
+#         eagle_count += 1
+#     else:
+#         tails_count += 1
+#
+# if eagle_count < tails_count:
+#     print(eagle_count)
+# elif eagle_count == tails_count:
+#     print(eagle_count)
+# else:
+#     print(tails_count)
+#############################
+# option 2
 
 n = int(input())
-eagle_count = tails_count = 0
-for n in range(n):
-    x = int(input())
-    if x == 1:
-        eagle_count += 1
-    elif x == 0:
-        tails_count += 1
-    else:
-        print('Invalid result')
-        break
-if eagle_count < tails_count:
-    print(eagle_count)
-elif eagle_count == tails_count:
-    print(eagle_count)
-else:
-    print(tails_count)
+count = 0
+for i in range(n):
+    eagle_coin = int(input())
+    if eagle_coin:
+        count += 1
+if count > n //2:
+    count = n - count
+print(count)
