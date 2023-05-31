bebe5210e0b57903d0afe9194b40e5166dcb171f
denisganiev3354 <denisganiev3354@gmail.com>
2023-05-31 22:14:00 +0400

    (L3)HWtask01

--- Lesson/lesson 3/HW/HWtask01.py
+++ Lesson/lesson 3/HW/HWtask01.py
@@ -8,4 +8,36 @@
 #     1 2 3 4 5
 #     3
 #     -> 1
+###########################################
+# мой вариант
 
+import random
+
+num = int(input())
+result = 0
+list_nums = []
+x = int(input())
+for i in range(num):
+    list_nums.append(random.randint(1, 5))
+    if list_nums[i] == x:
+        result += 1
+print(list_nums)
+print(result)
+
+#######################################
+# варианты преподавателя
+# вариант 1
+
+# list_nums = [int(input()) for _ in range(int(input()))]
+# print(list_nums.count(int(input())))
+
+# вариант 2
+
+# from random import choices
+#
+# num = int(input())
+# list_nums = choices(range(num * 2), k=num)
+# print(list_nums)
+#
+# result = list_nums.count(int(input()))
+# print(result)
