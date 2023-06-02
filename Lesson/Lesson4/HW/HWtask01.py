ca100f1c8327fd3eec72ddf3d50919521d06b188
denisganiev3354 <denisganiev3354@gmail.com>
2023-06-02 21:27:55 +0400

    L4HWtask01

--- Lesson/Lesson4/HW/HWtask01.py
+++ Lesson/Lesson4/HW/HWtask01.py
@@ -2,3 +2,27 @@
 # Выдать без повторений в порядке возрастания все те числа, которые встречаются в обоих наборах.
 # Пользователь вводит 2 числа. n — кол-во элементов первого множества.
 # m — кол-во элементов второго множества. Затем пользователь вводит сами элементы множеств.
+
+from random import randint
+
+n = int(input())
+list_num1 = []
+
+for i in range(n):
+    list_num1.append(randint(1, 10))
+print(list_num1)
+
+# print(list_num1[int(input())], list_num1.append(randint(1, 10)))
+
+m = int(input())
+list_num2 = []
+
+for i in range(m):
+    list_num2.append(randint(1, 10))
+print(list_num2)
+
+# print(list_num1[int(input())], list_num1.append(randint(1, 10)))
+
+l_n1 = set(list_num1)
+u = l_n1.union(list_num2)
+print(*u)
