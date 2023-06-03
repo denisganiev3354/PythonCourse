cb9bd3682c62eddf1e192db59b1e1d31f3a7f443
denisganiev3354 <denisganiev3354@gmail.com>
2023-06-03 18:00:15 +0400

    L4task01

--- Lesson/Lesson4/HW/HWtask01.py
+++ Lesson/Lesson4/HW/HWtask01.py
@@ -5,24 +5,26 @@
 
 from random import randint
 
-n = int(input())
-list_num1 = []
-
-for i in range(n):
-    list_num1.append(randint(1, 10))
-print(list_num1)
-
-# print(list_num1[int(input())], list_num1.append(randint(1, 10)))
-
-m = int(input())
-list_num2 = []
-
-for i in range(m):
-    list_num2.append(randint(1, 10))
-print(list_num2)
+# n = int(input())
+# list_num1 = []
+#
+# for i in range(n):
+#     list_num1.append(randint(1, 10))
+# print(list_num1)
 
 # print(list_num1[int(input())], list_num1.append(randint(1, 10)))
 
-l_n1 = set(list_num1)
-u = l_n1.union(list_num2)
+# m = int(input())
+# list_num2 = []
+#
+# for i in range(m):
+#     list_num2.append(randint(1, 10))
+# print(list_num2)
+#
+# # print(list_num1[int(input())], list_num1.append(randint(1, 10)))
+
+print(list_1 := [randint(1, 10) for i in range((int(input())))])
+print(list_2 := [randint(1, 10) for i in range((int(input())))])
+l_n1 = set(list_1)
+u = l_n1.union(list_2)
 print(*u)
