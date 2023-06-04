b39e82567f835c83992e5775c45482f8c3cd2c9f
denisganiev3354 <denisganiev3354@gmail.com>
2023-06-04 20:58:59 +0400

    L5HWtask01

---
+++ Lesson/Lesson5/HW/HWtask01.py
@@ -0,0 +1,22 @@
+# Задача 26:  Напишите программу, которая на вход принимает два числа A и B,
+# и возводит число А в целую степень B с помощью рекурсии.
+#
+# *Пример:*
+#
+# A = 3; B = 5 -> 243 (3⁵)
+#     A = 2; B = 3 -> 8
+
+
+def ExponentiationRec(num1, num2):
+    if num2 == 1:
+        return num1
+    elif num2 == 0:
+        return 1
+    elif num2 != 1:
+        return (num1 * ExponentiationRec(num1, num2 - 1))
+
+a = int(input())
+b = int(input())
+
+print(ExponentiationRec(a, b))
+
