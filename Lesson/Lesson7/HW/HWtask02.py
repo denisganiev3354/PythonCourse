f3cc32000374a5248c3cb77acc24d385aa2ae589
denisganiev3354 <denisganiev3354@gmail.com>
2023-06-13 21:22:10 +0400

    redact

--- Lesson/Lesson7/HW/HWtask02.py
+++ Lesson/Lesson7/HW/HWtask02.py
@@ -20,7 +20,7 @@
 def print_operation_table(operation, num_rows=6, num_columns=6):
     for i in range(1, num_rows + 1):
         for j in range(1, num_columns + 1):
-            print(operation(i, j), end=' ')
+            print(f'{operation(i, j): 3}', end=' ')
         print()
 
 print_operation_table(lambda x, y: x * y)
